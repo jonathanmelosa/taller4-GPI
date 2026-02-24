ad2b7c02c659c090fc4bf44bbc5910fba2f6daa3
Jonathan Melo <jonathan.melo5@gmail.com>
2026-02-24 10:49:09 -0500

    mejora en la visualización

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -11,7 +11,7 @@ hist(df$ingreso,
      main = "Histograma de ingreso (datos sintéticos)",
      xlab = "Ingreso",
      ylab = "Frecuencia"
-     col = "green",      
+     col = "black",      
      border = "white")
 dev.off()
 
